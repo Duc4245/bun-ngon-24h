5ad3cab5c4e3f3e9faa15bf60cccea8679389faf
Duc4245 <duchoang0919@gmail.com>
2026-04-14 02:58:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
-const API_URL = location.hostname === "localhost"
-  ? "http://localhost:3000"
-  : "https://bun-ngon-24.onrender.com";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "API_KEY_CỦA_BẠN",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
